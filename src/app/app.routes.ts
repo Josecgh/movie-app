9b6b909e31169c30fc5d778ff258a4a6adb14e0d
Jose Luis <jlcgmieres15@gmail.com>
2026-05-26 13:01:10 +0200

    Creating Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { MovieList } from './component/movie-list/movie-list';
+import { MovieDetails } from './component/movie-details/movie-details';
+import { NotFound } from './component/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home},
+  { path: 'home', redirectTo: '', component: Home, pathMatch: 'full' },
+  { path: 'movies', component: MovieList },
+  { path: 'movies/:movieId', component: MovieDetails },
+  { path: '**', component: NotFound }
+];
